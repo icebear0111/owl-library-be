d0b41f9f680f4f64991a46617dd640237d9b25f4
icebear0111 <wltjq0901@naver.com>
2025-02-21 17:43:03 +0900

    project setup

---
+++ src/test/java/com/project/owlibrary/OwlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.owlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
